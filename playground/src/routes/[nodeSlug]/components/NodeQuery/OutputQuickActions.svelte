c23a9c44b4d47eefe54f0f9fd7a3581cf6cbacee
Luis Eduardo <eduardo.devop@gmail.com>
2025-09-30 03:29:02 +0000

    refactor: adjust padding in OutputQuickActions for consistent spacing

--- playground/src/routes/[nodeSlug]/components/NodeQuery/OutputQuickActions.svelte
+++ playground/src/routes/[nodeSlug]/components/NodeQuery/OutputQuickActions.svelte
@@ -42,7 +42,7 @@
 
     {#each firstTokens as token}
       {#snippet menuContent()}
-        <div class="flex flex-col space-y-2 pt-2">
+        <div class="flex flex-col space-y-2 py-2">
           <button
             class="btn btn-sm btn-ghost w-full justify-start"
             onclick={() => handleCopyToClipboard(token)}
