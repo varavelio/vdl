2495baabacf34addbe9e66d47e2ea9b1ffc4b00d
Luis Eduardo <eduardo.devop@gmail.com>
2026-01-30 22:16:34 +0000

    Add --log-path flag to print LSP log file path

--- toolchain/cmd/vdl/cmd_lsp.go
+++ toolchain/cmd/vdl/cmd_lsp.go
@@ -1,15 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
+	"slices"
 
 	"github.com/varavelio/vdl/toolchain/internal/lsp"
 )
 
-type cmdLSPArgs struct{}
+type cmdLSPArgs struct {
+	LogPath bool `arg:"--log-path" help:"Print the path to the LSP log file"`
+}
 
 func cmdLSP(_ *cmdLSPArgs) {
+	// Manual parsing for flags that need to bypass the strict lsp mode
+	if slices.Contains(os.Args, "--log-path") {
+		fmt.Println(lsp.GetLogFilePath())
+		return
+	}
+
 	lspInstance := lsp.New(os.Stdin, os.Stdout)
 	if err := lspInstance.Run(); err != nil {
 		log.Fatalf("failed to run lsp: %s", err)
