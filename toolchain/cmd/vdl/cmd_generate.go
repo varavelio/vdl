7e99f279782dc42644abe88672a12951ad506a8d
Luis Eduardo <eduardo.devop@gmail.com>
2026-02-02 05:45:22 +0000

    Enhance VDL error output with ANSI colors and better formatting

--- toolchain/cmd/vdl/cmd_generate.go
+++ toolchain/cmd/vdl/cmd_generate.go
@@ -3,10 +3,12 @@ package main
 import (
 	"fmt"
 	"os"
+	"regexp"
 	"strings"
 	"time"
 
 	"github.com/varavelio/vdl/toolchain/internal/codegen"
+	"github.com/varavelio/vdl/toolchain/internal/util/cliutil"
 )
 
 type cmdGenerateArgs struct {
@@ -33,7 +35,27 @@ func cmdGenerate(args *cmdGenerateArgs) {
 
 	fileCount, err := codegen.Run(args.ConfigPath)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "VDL error: %v\n", err)
+		errStr := "VDL error: " + err.Error()
+
+		// Make the first line red bold
+		if idx := strings.Index(errStr, "\n"); idx != -1 {
+			errStr = cliutil.ColorizeRedBold(errStr[:idx]) + errStr[idx:]
+		} else {
+			errStr = cliutil.ColorizeRedBold(errStr)
+		}
+
+		// Add 2 spaces after each newline for better indentation
+		errStr = strings.ReplaceAll(errStr, "\n", "\n  ")
+
+		// Paint error[XXXX] patterns in red
+		errorCodePattern := regexp.MustCompile(`error\[[^\]]+\]`)
+		errStr = errorCodePattern.ReplaceAllStringFunc(errStr, cliutil.ColorizeRed)
+
+		// Make "did you mean ... ?" patterns bold
+		didYouMeanPattern := regexp.MustCompile(`did you mean[^?]+\?`)
+		errStr = didYouMeanPattern.ReplaceAllStringFunc(errStr, cliutil.ColorizeCyan)
+
+		fmt.Fprintf(os.Stderr, "%s\n", errStr)
 		os.Exit(1)
 	}
 
