596a4dcc6ef82d76d030187694d12ab0a7394986
Luis Eduardo <eduardo.devop@gmail.com>
2026-02-03 21:31:25 +0000

    Copy toolchain wasm files to docs build

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,9 +1,26 @@
 // @ts-check
 import starlight from "@astrojs/starlight";
 import { defineConfig } from "astro/config";
+import { viteStaticCopy } from "vite-plugin-static-copy";
 
 // https://astro.build/config
 export default defineConfig({
+  vite: {
+    plugins: [
+      viteStaticCopy({
+        targets: [
+          {
+            src: "../toolchain/dist/vdl.wasm",
+            dest: "_astro/vdl",
+          },
+          {
+            src: "../toolchain/dist/wasm_exec.js",
+            dest: "_astro/vdl",
+          },
+        ],
+      }),
+    ],
+  },
   integrations: [
     starlight({
       title: "My Docs",
