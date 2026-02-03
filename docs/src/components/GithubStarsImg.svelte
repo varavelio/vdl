32d727ef62e7c5631295e6c9cba1294555f95b01
Luis Eduardo <eduardo.devop@gmail.com>
2026-02-03 23:14:06 +0000

    Add github stars component to docs

---
+++ docs/src/components/GithubStarsImg.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  interface Props {
+    class?: string;
+  }
+
+  const { class: className = "" }: Props = $props();
+</script>
+
+<img
+  alt="GitHub Repo stars"
+  src="https://img.shields.io/github/stars/varavelio/vdl?style=plastic&label=%20"
+  class={className}
+/>
