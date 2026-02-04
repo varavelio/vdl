6e8609621fb6024a4ca9812fbbdca409879bf56b
Luis Eduardo <eduardo.devop@gmail.com>
2026-02-04 01:13:35 +0000

    Add getVersion helper

--- playground/src/lib/version.ts
+++ playground/src/lib/version.ts
@@ -1,6 +1,16 @@
 /**
  * version is replaced during the release process by the latest Git tag
- * and should not be manually edited.
+ * and should not be manually edited. Should not include the v prefix.
  */
-export const version = "0.0.0-dev";
-export const versionWithPrefix = `v${version}`;
+const version = "0.0.0-dev";
+
+/**
+ * Takes the playground version, cleans it and returns it
+ *
+ * @returns cleaned playground version without v prefix
+ */
+export function getVersion(): string {
+  let ver = version.toLowerCase().trim();
+  if (!ver.startsWith("v")) return ver;
+  return ver.slice(1);
+}
