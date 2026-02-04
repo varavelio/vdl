533039ff6124f5d7c440c57e07476fa2b22aaba2
Luis Eduardo <eduardo.devop@gmail.com>
2026-02-04 17:38:33 +0000

    Show LayoutSwaggerSwitch only when there are RPCs defined in the schema

--- playground/src/routes/components/LayoutSwaggerSwitch.svelte
+++ playground/src/routes/components/LayoutSwaggerSwitch.svelte
@@ -1,6 +1,8 @@
 <script lang="ts">
   import { fade } from "svelte/transition";
 
+  import { storeSettings } from "$lib/storeSettings.svelte";
+
   import Logo from "$lib/components/Logo.svelte";
   import SwaggerLogo from "$lib/components/SwaggerLogo.svelte";
   import Tooltip from "$lib/components/Tooltip.svelte";
@@ -16,6 +18,8 @@
     isOpen ? "Switch to VDL" : "Switch to Swagger UI (OpenAPI)",
   );
 
+  let shouldShow = $derived(storeSettings.store.irSchema.rpcs.length > 0);
+
   const handleEscapeKey = (event: KeyboardEvent) => {
     if (!isOpen) return;
     if (event.key === "Escape") toggle();
@@ -28,35 +32,37 @@
   });
 </script>
 
-<Tooltip content={tooltipContent} placement="left">
-  <button
-    class={{
-      "group btn btn-circle btn-lg fixed right-4 bottom-4 z-50": true,
-      "bg-base-300 border-base-content/20": isOpen,
-      "btn-ghost bg-transparent": !isOpen,
-    }}
-    onclick={toggle}
-  >
-    {#if !isOpen}
-      <span in:fade={{ duration: animationDuration }}>
-        <SwaggerLogo class="w-full" />
-      </span>
-    {/if}
-    {#if isOpen}
-      <span in:fade={{ duration: animationDuration }}>
-        <Logo class="size-10" />
-      </span>
-    {/if}
-  </button>
-</Tooltip>
+{#if shouldShow}
+  <Tooltip content={tooltipContent} placement="left">
+    <button
+      class={{
+        "group btn btn-circle btn-lg fixed right-4 bottom-4 z-50": true,
+        "bg-base-300 border-base-content/20": isOpen,
+        "btn-ghost bg-transparent": !isOpen,
+      }}
+      onclick={toggle}
+    >
+      {#if !isOpen}
+        <span in:fade={{ duration: animationDuration }}>
+          <SwaggerLogo class="w-full" />
+        </span>
+      {/if}
+      {#if isOpen}
+        <span in:fade={{ duration: animationDuration }}>
+          <Logo class="size-10" />
+        </span>
+      {/if}
+    </button>
+  </Tooltip>
 
-<div
-  class={{
-    "bg-base-100 fixed top-0 left-0 z-40 h-[100dvh] w-[100dvw] overflow-y-auto": true,
-    "transition-transform": true,
-    "translate-y-[100dvh]": !isOpen,
-  }}
-  style="transition-duration: {animationDuration}ms;"
->
-  <LayoutSwaggerUi onClose={toggle} />
-</div>
+  <div
+    class={{
+      "bg-base-100 fixed top-0 left-0 z-40 h-dvh w-dvw overflow-y-auto": true,
+      "transition-transform": true,
+      "translate-y-[100dvh]": !isOpen,
+    }}
+    style="transition-duration: {animationDuration}ms;"
+  >
+    <LayoutSwaggerUi onClose={toggle} />
+  </div>
+{/if}
