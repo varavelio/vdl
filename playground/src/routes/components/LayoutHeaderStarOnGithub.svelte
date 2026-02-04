0c40c7313a5e234eda69f0a36595a563b7cc3d75
Luis Eduardo <eduardo.devop@gmail.com>
2026-02-04 17:40:13 +0000

    Update GitHub stars badge to point to varavelio/vdl repository

--- playground/src/routes/components/LayoutHeaderStarOnGithub.svelte
+++ playground/src/routes/components/LayoutHeaderStarOnGithub.svelte
@@ -11,6 +11,6 @@
   <span>Star on GitHub</span>
   <img
     alt="GitHub Repo stars"
-    src="https://img.shields.io/github/stars/uforg/uforpc?style=plastic&label=%20"
+    src="https://img.shields.io/github/stars/varavelio/vdl?style=plastic&label=%20"
   />
 </a>
